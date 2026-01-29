27389ae82606ef24497c849137f89ced143e9159
Rebecka Kinn <rebeckaki@getacademy.no>
2026-01-29 11:10:48 +0100

    Added files

---
+++ script.js
@@ -0,0 +1,62 @@
+let password = '';
+let username = '';
+
+let correctPassword = '123abc';
+let correctUsername = 'Rebecka';
+
+let message = '';
+
+updateView();
+function updateView(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+        <div>
+            <label>Brukernavn</label>
+            <input onchange="saveUsername(this.value)" type="text" placeholder="Skriv inn brukernavn" value=${username}>
+
+            <label>Passord</label>
+            <input onchange="savePassword(this.value)" type="password" placeholder="Skriv inn passord" value=${password}> 
+
+            <button onclick="logIn()">Logg inn</button>
+            <p>${message}</p>
+        </div>
+    
+    `;
+}
+
+function saveUsername(input){
+    console.log("username:", input);
+    username = input;
+}
+function savePassword(input){
+    console.log("password:", input);
+    password = input;
+}
+
+function logIn(){
+    if(password == correctPassword && username == correctUsername){
+        profileView();
+    }else if(password != correctPassword || username != correctUsername){
+        message = "Passord eller brukernavn er feil.";
+        updateView();
+    }else{
+        message = "Error";
+        updateView();
+    }
+}
+
+function logOut(){
+    password = '';
+    username = '';
+    updateView();
+}
+
+function profileView(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+        <div>
+            <h1>Velkommen ${correctUsername}!</h1>
+            <button onclick="logOut()">Logg ut</button>
+        </div>
+    
+    `;
+}
+
