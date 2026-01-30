a420fa5cb77e68c6b50e413072837939acc39f58
Rebecka Kinn <rebeckaki@getacademy.no>
2026-01-30 09:27:30 +0100

    creating startup

--- undervisning_30_01/script/main.js
+++ undervisning_30_01/script/main.js
@@ -1,1 +1,11 @@
-console.log("works")
+const page = document.getElementById('app');
+
+
+updateView();
+function updateView(){
+    page.innerHTML = /*HTML*/`
+        <div>
+            <p>Hei</p>
+        </div>
+    `;
+}
