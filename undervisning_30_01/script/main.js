4c1bd480c90fbd46a3431a70554f9e63e582c8ed
Rebecka Kinn <rebeckaki@getacademy.no>
2026-01-30 11:16:13 +0100

    making it nice

--- undervisning_30_01/script/main.js
+++ undervisning_30_01/script/main.js
@@ -1,11 +1,22 @@
 const page = document.getElementById('app');
 
+let correctPassword = '123abc';
+let correctUsername = 'admin';
+let password = '';
+let username = '';
+
 
 updateView();
 function updateView(){
     page.innerHTML = /*HTML*/`
-        <div>
-            <p>Hei</p>
-        </div>
+        <section>
+            <label for="username">Username</label>
+            <input id="username" type="text" placeholder="Your username">
+            
+            <label for="password">Password</label>
+            <input id="password" type="password" placeholder="Your password">
+
+            <button>Log in</button>
+        </section>
     `;
 }
