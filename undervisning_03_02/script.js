a1d9f888c9996174aa6ec4efa433c4c5a41ca829
Rebecka Kinn <rebeckaki@getacademy.no>
2026-02-03 11:24:17 +0100

    ed

---
+++ undervisning_03_02/script.js
@@ -0,0 +1,13 @@
+const page = document.getElementById('app');
+let currentPage = '';
+
+//log-in page
+//main page
+//dice roll with input
+
+updateView();
+function updateView(){
+    let html = '';
+    
+    page.innerHTML = html;
+}
