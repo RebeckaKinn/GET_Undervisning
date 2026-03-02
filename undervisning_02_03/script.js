7a717b72097c794fa2e425cd473df2b30b722387
Rebecka Kinn <rebeckaki@getacademy.no>
2026-03-02 14:59:21 +0100

    gjør oppgaven klar

---
+++ undervisning_02_03/script.js
@@ -0,0 +1,10 @@
+/*
+Tema: Loops og Arrays
+
+To-Do-app
+
+- MVC
+- Et array med oppgaver, kan legge til flere elementer. 
+- Trykke på dem for å fjerne. 
+- Søke etter oppgave (filtrere) - som dynamisk kommer opp per bokstav. 
+*/
