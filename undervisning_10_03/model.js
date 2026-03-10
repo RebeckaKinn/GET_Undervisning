35236bd76127d5c6592ae42ae8cbadf2835ea02a
Rebecka Kinn <rebeckaki@getacademy.no>
2026-03-10 11:28:40 +0100

    undervisning ferdig yay

--- undervisning_10_03/model.js
+++ undervisning_10_03/model.js
@@ -5,3 +5,42 @@ MODELL:
         - VIEWSTATE (input)
         - DATA
  */
+
+const model = {
+  app: {
+    app: document.getElementById("app"),
+    loggedInId: null,
+    currentPage: "createNewUser",
+  },
+  viewState: {
+    logIn: {
+      username: null,
+      password: null,
+    },
+    newUser: {
+      id: null,
+      username: null,
+      password: null,
+      firstName: null,
+      lastName: null,
+    },
+  },
+  data: {
+    users: [
+      {
+        id: 0,
+        username: "Admin",
+        password: "123abc",
+        firstName: "Kåre",
+        lastName: "Kåresen",
+      },
+      {
+        id: 1,
+        username: "Mona",
+        password: "abc123",
+        firstName: "Mona",
+        lastName: "Monesen",
+      },
+    ],
+  },
+};
